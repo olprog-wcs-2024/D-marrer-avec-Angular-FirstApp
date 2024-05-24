5cc50b13a3dd2f4ee3268fc3043abea928086a48
gaouimounir <mounir.gaoui@outlook.com>
2024-05-24 17:14:22 +0200

    feat: :art: Challenge 💪 Des boîtes aux quatre coins 📦

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlockComponent } from './block/block.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BlockComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
