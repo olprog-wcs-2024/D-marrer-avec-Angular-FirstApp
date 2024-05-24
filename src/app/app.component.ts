4686b93b717c38427dc831c88fde6e3021c8185b
gaouimounir <mounir.gaoui@outlook.com>
2024-05-24 15:37:54 +0200

    feat: :art: 💪 Challenge Application Angular sur Github 🛡

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myFirstApp';
+  title = 'Bienvenue sur le site de Mounir !';
 }
