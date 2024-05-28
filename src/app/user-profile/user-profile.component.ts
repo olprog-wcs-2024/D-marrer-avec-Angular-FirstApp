3b3d6e2b29a944058f07bf24d88dee1c28f1baa0
gaouimounir <mounir.gaoui@outlook.com>
2024-05-28 11:27:56 +0200

    feat: :zap: creation de l'objet user

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+
+  user = {
+    name: 'Doe',
+    firstName: 'John',
+    age : 25,
+    quote: '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  }
 
 }
