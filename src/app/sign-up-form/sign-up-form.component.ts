4344079c37cffca2ef25bc7f949cb94e25a5d4d8
gaouimounir <mounir.gaoui@outlook.com>
2024-05-29 16:13:15 +0200

    feat: :zap: utilisation de ngModel dans le formulaire

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Order } from '../models/order.models';
 
 @Component({
   selector: 'app-sign-up-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,],
   templateUrl: './sign-up-form.component.html',
   styleUrl: './sign-up-form.component.css'
 })
 export class SignUpFormComponent {
 
+    newOrder: Order = {
+      title: "",
+      quantity: 0,
+      date: new Date(),
+      contact: ""
+    };
+
   onSubmit(): void {
     // form submitted
-    console.log('Formulaire envoyé !');
+    console.log(this.newOrder);
   }
-  
+
 }
