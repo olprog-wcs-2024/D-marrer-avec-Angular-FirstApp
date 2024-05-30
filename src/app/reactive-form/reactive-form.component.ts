0a8c416819d1b6a8266e987aa9801050e1f101ba
gaouimounir <mounir.gaoui@outlook.com>
2024-05-30 16:19:21 +0200

    feat: :zap: utilisation du form builder et validators dans le ts

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
 import { Movie } from '../models/movie.models';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, Validators, FormBuilder],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
 
-  newMovie: Movie = {
-    id: '',
-    title: '',
-    type: '',
-    yearRelease: 0,
-    fiche: ''
-  };
+  movieForm = this.fb.group({
+    id: ['', Validators.required],
+    title: ['', Validators.required],
+    type: ['', Validators.required],
+    yearRelease: ['', Validators.required],
+    fiche: ['', Validators.required],
+
+  });
+
+  constructor(private fb: FormBuilder) {}
 
   onSubmit() {
     // Form submitted
-    console.log(this.newMovie);
+    console.log('movieForm : submitted', this.movieForm.value);
   }
 }
