c51a81537ec4dac9f540361c10ae5657573a2eb9
gaouimounir <mounir.gaoui@outlook.com>
2024-05-31 17:03:25 +0200

    fix: :bug: affichage du formulaire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserAdminComponent } from './user-admin/user-admin.component';
 import { MenuComponent } from './menu/menu.component';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,BlockComponent,UserProfileComponent,UserAdminComponent,MenuComponent,SignUpFormComponent],
+  imports: [RouterOutlet,RouterLink,BlockComponent,UserProfileComponent,UserAdminComponent,MenuComponent,SignUpFormComponent,ReactiveFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { Movie } from '../models/movie.models';
-import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, Validators, ReactiveFormsModule, FormArray } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
-  imports: [ReactiveFormsModule, Validators, FormBuilder],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
 
   movieForm = this.fb.group({
+
+    // declare all controls with validation rules
+
     id: ['', Validators.required],
     title: ['', Validators.required],
     type: ['', Validators.required],
@@ -26,5 +30,15 @@ export class ReactiveFormComponent {
   onSubmit() {
     // Form submitted
     console.log('movieForm : submitted', this.movieForm.value);
+  }
+
+
+  ngOnInit() {
+    // get Observable from FormGroup
+    this.movieForm.valueChanges
+      // listen to value change
+      .subscribe(value => {
+        console.log('orderForm value changes : ', value);
+      });
   }
 }
