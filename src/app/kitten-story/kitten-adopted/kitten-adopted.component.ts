0172a3f46f3e52d4e3d35d2e94a3912e34fde815
gaouimounir <mounir.gaoui@outlook.com>
2024-06-25 12:00:39 +0200

    create adopted kitten

--- src/app/kitten-story/kitten-adopted/kitten-adopted.component.ts
+++ src/app/kitten-story/kitten-adopted/kitten-adopted.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { KittenService, Kitten } from '../../services/kitten.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-kitten-adopted',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './kitten-adopted.component.html',
   styleUrl: './kitten-adopted.component.css'
 })
-export class KittenAdoptedComponent {
 
+export class AdoptedKittenListComponent {
+  adoptedKittens: Kitten[];
+
+  constructor(private kittenService: KittenService) {
+    this.adoptedKittens = this.kittenService.getAdoptedKittens();
+  }
 }
