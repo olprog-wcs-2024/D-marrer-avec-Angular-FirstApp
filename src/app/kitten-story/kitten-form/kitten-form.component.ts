e6b737f5d2e8a2e4b5a140bc8bcb90b490f15a26
gaouimounir <mounir.gaoui@outlook.com>
2024-06-25 11:53:16 +0200

    create kitten form

--- src/app/kitten-story/kitten-form/kitten-form.component.ts
+++ src/app/kitten-story/kitten-form/kitten-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { KittenService, Kitten } from '../../services/kitten.service';
 
 @Component({
   selector: 'app-kitten-form',
@@ -8,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './kitten-form.component.css'
 })
 export class KittenFormComponent {
+  name = '';
+  breed = '';
+  birthDate = '';
+  imageUrl = '';
+  isValid = false;
 
+  constructor(private kittenService: KittenService) {}
+
+  validateForm() {
+    this.isValid = this.name !== '' && this.breed !== '' && this.birthDate !== '' && this.imageUrl !== '';
+  }
+
+  addKitten() {
+    if (this.isValid) {
+      const newKitten: Kitten = {
+        name: this.name,
+        breed: this.breed,
+        birthDate: this.birthDate,
+        imageUrl: this.imageUrl
+      };
+      this.kittenService.addKitten(newKitten);
+      this.name = '';
+      this.breed = '';
+      this.birthDate = '';
+      this.imageUrl = '';
+      this.isValid = false;
+    }
+  }
 }
